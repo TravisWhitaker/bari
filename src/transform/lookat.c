75d52a416db6a1a4b0511b2da38524d373c5d94b
Travis Whitaker <pi.boy.travis@gmail.com>
2015-12-27 16:39:04 -0500

    Fixed sign error.

--- src/transform/lookat.c
+++ src/transform/lookat.c
@@ -6,18 +6,18 @@
 bari_mat4 bari_lookat(bari_vec3 eye, bari_vec3 center, bari_vec3 up)
 {
 	bari_mat4 r = bari_mat4_zero;
-	bari_vec3 za = bari_norm3(bari_vsub3(center, eye));
+	bari_vec3 za = bari_norm3(bari_vsub3(eye, center));
 	bari_vec3 xa = bari_norm3(bari_cross(za, up));
 	bari_vec3 ya = bari_cross(xa, za);
 	r.xx = xa.x;
 	r.yx = ya.x;
-	r.zx = -za.x;
+	r.zx = za.x;
 	r.xy = xa.y;
 	r.yy = ya.y;
-	r.zy = -za.y;
+	r.zy = za.y;
 	r.xz = xa.z;
 	r.yz = ya.x;
-	r.zz = -za.z;
+	r.zz = za.z;
 	r.xw = -bari_dot3(xa, eye);
 	r.yw = -bari_dot3(ya, eye);
 	r.zw = bari_dot3(za, eye);
