ea2bcd75f0d2dac527ab51aabe1a36e7ecf72bb1
Travis Whitaker <pi.boy.travis@gmail.com>
2015-12-27 16:22:58 -0500

    Add lookAt function.

---
+++ src/transform/lookat.c
@@ -0,0 +1,26 @@
+// libbari Copyright (C) Travis Whitaker 2015-2016
+
+#include <bari.h>
+
+// Someone please check this math.
+bari_mat4 bari_lookat(bari_vec3 eye, bari_vec3 center, bari_vec3 up)
+{
+	bari_mat4 r = bari_mat4_zero;
+	bari_vec3 za = bari_norm3(bari_vsub3(center, eye));
+	bari_vec3 xa = bari_norm3(bari_cross(za, up));
+	bari_vec3 ya = bari_cross(xa, za);
+	r.xx = xa.x;
+	r.yx = ya.x;
+	r.zx = -za.x;
+	r.xy = xa.y;
+	r.yy = ya.y;
+	r.zy = -za.y;
+	r.xz = xa.z;
+	r.yz = ya.x;
+	r.zz = -za.z;
+	r.xw = -bari_dot3(xa, eye);
+	r.yw = -bari_dot3(ya, eye);
+	r.zw = bari_dot3(za, eye);
+	r.ww = 1;
+	return r;
+}
