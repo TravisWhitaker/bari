a37c425537de41e8c84a9ebe477ac48e48e62712
Travis Whitaker <pi.boy.travis@gmail.com>
2015-12-27 17:14:23 -0500

    Add infinite perspective and orthographic projections.

---
+++ src/transform/infinite_perspective.c
@@ -0,0 +1,18 @@
+// libbari Copyright (C) Travis Whitker 2015-2016
+
+#include <bari.h>
+
+#include <math.h>
+
+// Someone please check this math.
+bari_mat4 bari_infinite_perspective(float fovy, float aspect, float near)
+{
+	bari_mat4 r = bari_mat4_zero;
+	float t = near*tanf(fovy / 2);
+	r.xx =  near / (aspect * t);
+	r.yy = near / t;
+	r.zz = -1;
+	r.zw = -2 * near;
+	r.wz = -1;
+	return r;
+}
---
+++ src/transform/ortho.c
@@ -0,0 +1,20 @@
+// libbari Copyright (C) Travis Whitker 2015-2016
+
+#include <bari.h>
+
+// Someone please check this math.
+bari_mat4 bari_ortho(float left, float right, float bottom, float top, float near, float far)
+{
+	bari_mat4 r = bari_mat4_zero;
+	float x = 1 / (left - right);
+	float y = 1 / (bottom - top);
+	float z = 1 / (near - far);
+	r.xx = -2 * x;
+	r.yy = -2 * y;
+	r.zz = 2 * z;
+	r.xw = (right + left) * x;
+	r.yw = (top + bottom) * y;
+	r.zw = (far + near) * z;
+	r.ww = 1;
+	return r;
+}
